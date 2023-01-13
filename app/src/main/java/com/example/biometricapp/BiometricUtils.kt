54232730f0bcf0fd391937898e41cd89e039a89c
Antonio Fernández Villa <fdez2002@gmail.com>
2023-01-13 13:52:04 +0100

    Creacion objeto

---
+++ app/src/main/java/com/example/biometricapp/BiometricUtils.kt
@@ -0,0 +1,75 @@
+package com.example.biometricapp
+
+import android.content.Context
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_WEAK
+import androidx.biometric.BiometricManager.BIOMETRIC_SUCCESS
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+
+
+interface BiometricAuthCallback {
+    fun onSuccess()
+    fun onError()
+    fun onNotRecognized()
+}
+/**
+ * Se definen los metodos que sean relativos a todo lo que se operaciones de biometria
+ * ej: metodo que diga que el dispositivo este listo para usar biometria
+ */
+object BiometricUtils {
+
+    //nos dice si el dispositivo permite biometria
+    fun isDeviceReady(context: Context): Boolean = getCapability(context) == BIOMETRIC_SUCCESS
+
+
+    //devulve si tiene capacidad de biometria
+    private fun getCapability(context: Context): Int =
+        BiometricManager.from(context).canAuthenticate(BIOMETRIC_WEAK)
+
+    //mostramos el promt de geometria
+    fun showPromopt(
+        title: String ="",
+        subtitle: String ="",
+        description: String= "",
+        cancelButton: String = "",
+        activity: AppCompatActivity,
+        callback: BiometricAuthCallback
+    ){
+        val promptInfo =
+            BiometricPrompt.PromptInfo.Builder()
+                .setTitle(title)
+                .setSubtitle(subtitle)
+                .setDescription(description)
+                .setAllowedAuthenticators(BIOMETRIC_WEAK)
+                .setNegativeButtonText(cancelButton)
+
+
+    }
+    private fun initPrompt(activity: AppCompatActivity, callback: BiometricAuthCallback):BiometricPrompt{
+        val executor = ContextCompat.getMainExecutor(activity)
+        val authenticationCallback = object : BiometricPrompt.AuthenticationCallback(){
+
+            override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                super.onAuthenticationSucceeded(result)
+                callback.onSuccess()
+            }
+            override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                super.onAuthenticationError(errorCode, errString)
+                callback.onError()
+            }
+            override fun onAuthenticationFailed() {
+                super.onAuthenticationFailed()
+                callback.onNotRecognized()
+            }
+        }
+        return BiometricPrompt(activity, executor, authenticationCallback)
+
+    }
+
+
+
+}
--- app/src/main/java/com/example/biometricapp/MainActivity.kt
+++ app/src/main/java/com/example/biometricapp/MainActivity.kt
@@ -2,10 +2,47 @@ package com.example.biometricapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.biometricapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //checkeo capacidades biometricas del dispositivo
+        checkBiometricCapability()
+        //muestra el prompt dr la biometria
+        showBiometricPrompt()
+        //rellenamos los datos
+        fillUserData()
+        binding.buttonSaveData.setOnClickListener {
+            //dejamos el boton escuhando eventos de click
+            saveUserData()
+        }
+
     }
+
+
+    private fun checkBiometricCapability() {
+       if (!BiometricUtils.isDeviceReady(this)){
+           finish()
+       }else{
+           Toast.makeText(this, "Biometric Disponible", Toast.LENGTH_LONG).show()
+       }
+    }
+
+    private fun showBiometricPrompt() {
+    }
+
+    private fun fillUserData() {
+    }
+
+    private fun saveUserData() {
+    }
+
+
 }
